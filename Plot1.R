78f34094a69d2fd28c9b694f90cd770a5ea1e120
sananiman <simanguliyev2017@ada.edu.az>
2020-04-06 16:14:42 +0400

    First commit

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+total_annual_emissions <- aggregate(Emissions ~ year, NEI, FUN = sum)
+color_range <- 2:(length(total_annual_emissions$year)+1)
+with(total_annual_emissions, 
+     barplot(height=Emissions/1000, names.arg = year, col = color_range, 
+             xlab = "Year", ylab = expression('PM'[2.5]*' in Kilotons'),
+             main = expression('Annual Emission PM'[2.5]*' from 1999 to 2008')))
